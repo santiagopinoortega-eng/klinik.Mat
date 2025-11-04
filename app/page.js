5841c984d4a23b3141f6baedbc893bc4102fb12f
santiagopinoortega-eng <santiagopinoortega@gmail.com>
2025-11-04 05:49:12 -0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,9 @@
+// Esto es un componente simple de React
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Klinik.mt está en línea</h1>
+      <p>El despliegue funcionó.</p>
+    </div>
+  );
+}
