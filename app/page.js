9efb3b88da06e21d74bad51c7930e9ca1fedc648
santiagopinoortega-eng <santiagopinoortega@gmail.com>
2025-11-04 05:52:24 -0300

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-// Esto es un componente simple de React
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Klinik.mt está en línea</h1>
-      <p>El despliegue funcionó.</p>
-    </div>
-  );
-}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Hola Mundo!</h1>
-      <p>Mi proyecto Klinik.mt está funcionando.</p>
-    </div>
-  );
-}
